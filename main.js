1101de36080d5852fa06136d1abc6cfc0f0185a6
synodical <lena0323park@naver.com>
2021-12-28 23:33:34 +0900

    template using module

--- main.js
+++ main.js
@@ -2,35 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-function templateHTML(title, list, body, control) {
-  return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-  </body>
-  </html>
-  `;
-}
-
-function templateList(filelist) {
-  var list = '<ul>';
-  var i = 0;
-  while (i < filelist.length) {
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-    i = i+1;
-  }
-  list = list + '<ul>';
-  return list;
-}
+var template = require('./lib/template.js');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -41,21 +13,21 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js!';
-          var list = templateList(filelist);
-          var template = templateHTML(title, list,
+          var list = template.list(filelist);
+          var html = template.HTML(title, list,
               `<h2>${title}</h2>${description}`,
               `<a href="/create">create</a>`
           );
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         })
       }
       else {
         fs.readdir('./data', function(error, filelist){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
             var title = queryData.id;
-            var list = templateList(filelist);
-            var template = templateHTML(title, list,
+            var list = template.list(filelist);
+            var html = template.HTML(title, list,
                 `<h2>${title}</h2>${description}`,
                 `<a href="/create">create</a>
                 <a href="/update?id=${title}">update</a>
@@ -66,7 +38,7 @@ var app = http.createServer(function(request,response){
                 `
             );
             response.writeHead(200);
-            response.end(template);
+            response.end(html);
           });
         });
       }
@@ -75,9 +47,9 @@ var app = http.createServer(function(request,response){
       fs.readdir('./data', function(error, filelist){
         var title = 'Web - create';
         var description = 'Hello, Node.js!';
-        var list = templateList(filelist);
-        var template = templateHTML(title, list, `
-          <form action="/create_process" method="post">
+        var list = template.list(filelist);
+        var html = template.HTML(title, list,
+          `<form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -88,7 +60,7 @@ var app = http.createServer(function(request,response){
           </form>
           `, '');
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       })
     }
     else if (pathname ==='/create_process') {
@@ -110,8 +82,8 @@ var app = http.createServer(function(request,response){
       fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var title = queryData.id;
-          var list = templateList(filelist);
-          var template = templateHTML(title, list, // update시 생성되는 폼
+          var list = template.list(filelist);
+          var html = template.HTML(title, list, // update시 생성되는 폼
               `
               <form action="/update_process" method="post">
                 <input type="hidden" name="id" value="${title}">
@@ -127,7 +99,7 @@ var app = http.createServer(function(request,response){
               `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
           );
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         });
       });
     }
