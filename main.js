05a819a7a60195b88294a97643e26a147ecff75e
synodical <lena0323park@naver.com>
2021-12-28 21:03:26 +0900

    delete article

--- main.js
+++ main.js
@@ -57,7 +57,13 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
                 `<h2>${title}</h2>${description}`,
-                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                `<a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>
+                `
             );
             response.writeHead(200);
             response.end(template);
@@ -71,7 +77,7 @@ var app = http.createServer(function(request,response){
         var description = 'Hello, Node.js!';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -105,13 +111,56 @@ var app = http.createServer(function(request,response){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list,
-              `<h2>${title}</h2>${description}`,
+          var template = templateHTML(title, list, // update시 생성되는 폼
+              `
+              <form action="/update_process" method="post">
+                <input type="hidden" name="id" value="${title}">
+                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                <p>
+                  <textarea name="description" placeholder="description"> ${description}"</textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>
+              `,
               `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
           );
           response.writeHead(200);
           response.end(template);
         });
+      });
+    }
+    else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error){ // 파일명 변경
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){ // 파일 내용 변경
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+          });
+      });
+    }
+    else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
       });
     }
     else {
